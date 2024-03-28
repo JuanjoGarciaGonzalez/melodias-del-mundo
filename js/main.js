b23a0653279f3b3303b964d0ada3e26090fd8fa7
JuanjoGarciaGonzalez <juadrigarciagonzalez@gmail.com>
2024-03-28 13:49:48 +0100

    services plans

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Change state menu hamburguer
     var checkbox = document.getElementById('bars');
     var menu = document.querySelector('.responsive-menu');
 
@@ -9,4 +10,34 @@ document.addEventListener('DOMContentLoaded', function () {
             menu.style.display = 'none';
         }
     });
+
+    // Change money value
+
+    let moneyCheckbox = document.getElementById('moneyCheckbox');
+
+    moneyCheckbox.addEventListener('change', changeMoney);
+
+    function changeMoney() {
+        var moneyValueBasic = document.getElementById('card-money-basic');
+        var moneyValueStandart = document.getElementById('card-money-standart');
+        var moneyValuePremium = document.getElementById('card-money-premium');
+
+        var monthText = document.getElementsByClassName('card-month');
+
+        if(moneyCheckbox.checked) {
+            moneyValueBasic.innerHTML = '49';
+            moneyValueStandart.innerHTML = '99';
+            moneyValuePremium.innerHTML = '129';
+            Array.prototype.forEach.call(monthText, function(el) {
+                el.innerHTML = 'YEAR';
+            });
+        }else {
+            moneyValueBasic.innerHTML = '4.99';
+            moneyValueStandart.innerHTML = '9.99';
+            moneyValuePremium.innerHTML = '12.99';
+            Array.prototype.forEach.call(monthText, function(el) {
+                el.innerHTML = 'MONTH';
+            });
+        }
+    }
 });
